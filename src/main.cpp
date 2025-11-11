35a6f010133ec604a87018caf26f982878b7ad34
potzplitz <janschilfahrt@gmail.com>
2025-11-11 10:58:29 +0100

    Blinkende LED Fertig

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,61 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+void readSerial();
+void blinkTask(void *parameter);
+
+int frequency = 500;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200); // Stellt Serielle Konsole Baudrate auf 115200
+
+  Serial.print(">"); // erster output für die Eingabe
+
+  xTaskCreate( // erstellt einen extra thread, damit das Blinken unabhänig von dem Main Loop ist
+    blinkTask,
+    "BlinkTask",
+    2048,
+    NULL,
+    1,
+    NULL
+  );
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  readSerial();
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void readSerial() {
+  static String buffer = "";
+
+  while (Serial.available()) {
+
+    char c = Serial.read();
+    Serial.print(c);
+
+    if (c == '\n') {
+
+      Serial.print("Neue Frequenz: ");
+      Serial.println(buffer);
+
+      frequency = buffer.toInt();
+      buffer = "";
+
+      Serial.print(">");
+    } else {
+      buffer += c;
+
+    }
+  }
+}
+
+void blinkTask(void *parameter) {
+
+  while(true) {
+    digitalWrite(LED_BUILTIN , HIGH);
+    vTaskDelay(frequency / portTICK_PERIOD_MS);
+
+    digitalWrite(LED_BUILTIN, LOW);
+    vTaskDelay(frequency / portTICK_PERIOD_MS);
+  }
 }
