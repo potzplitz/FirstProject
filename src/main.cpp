0c1c249fe62aefabdb13414ead4e2510e7f88f4c
potzplitz <janschilfahrt@gmail.com>
2025-11-25 09:52:31 +0100

    WLAN Access Point

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,20 @@
 #include <Arduino.h>
+#include <Wifi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char *ssid = "WLAN 67676767";
+const char *password = "123456789";
+const int ap_channel = 6;
+const boolean ap_hidden = false;
+
+IPAddress local_ip(192,168,4,100);
+IPAddress gateway(192,168,4,254);
+IPAddress netmask(255,255,255,0);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  WiFi.softAPConfig(local_ip, gateway, netmask);
+  WiFi.softAP(ssid, password, ap_channel, ap_hidden);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  delay(1000);
 }
