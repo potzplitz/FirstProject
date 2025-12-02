f111a4f7b034b9a5411d38708dda734ad5ab9591
potzplitz <janschilfahrt@gmail.com>
2025-12-02 09:24:39 +0100

    webserver

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <Arduino.h>
 #include <Wifi.h>
+#include <WebServer.h>
 
-const char *ssid = "WLAN 67676767";
+void handleRoot();
+void handleNotFound();
+
+const char *ssid = "Jan_Sch";
 const char *password = "123456789";
 const int ap_channel = 6;
 const boolean ap_hidden = false;
@@ -10,11 +14,41 @@ IPAddress local_ip(192,168,4,100);
 IPAddress gateway(192,168,4,254);
 IPAddress netmask(255,255,255,0);
 
+WebServer server(80);
+
+const char StartPage[] PROGMEM = R"htmlgrenze(
+  <html>
+  <body>
+    <div style="display: flex; flex-direction: row; justify-content: center; align-items: center; width: 100%; height; 100%;">
+      <div style="width: 300px; height: 150px; border: 1px solid black; border-radius: 7px;">
+        <h1 style="width: 100%; text-align: center;">Willkommen!</h1>
+        <p style="width: 100%; height: auto; text-align: center;">Teststartseite vom ESP32!!</p>
+        <p style="width: 100%; height: auto; text-align: center; color: lightgray;">bloß dass die halt noch nichts kann...</p>
+      </div>
+    </div>
+  </body>
+  </html>
+s)htmlgrenze";
+
 void setup() {
   WiFi.softAPConfig(local_ip, gateway, netmask);
-  WiFi.softAP(ssid, password, ap_channel, ap_hidden);
+  boolean result = WiFi.softAP(ssid, password, ap_channel, ap_hidden);
+
+  if(result) {
+    server.on("/", handleRoot);
+    server.onNotFound(handleNotFound);
+    server.begin();
+  }
 }
 
 void loop() {
-  delay(1000);
+  server.handleClient();
+}
+
+void handleRoot() {
+  server.send(200, "text/html", StartPage);
+}
+
+void handleNotFound() {
+  server.send(404, "text/plain", "404 not found!!!");
 }
